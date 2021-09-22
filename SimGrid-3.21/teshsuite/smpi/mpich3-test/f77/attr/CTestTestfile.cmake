0b938539bf700bc669c2504736b8743cf5c081b2
Kien Pham <kien@sin1.isnet>
2021-09-23 00:11:51 +0900

    Modify gitignore the last time

--- SimGrid-3.21/teshsuite/smpi/mpich3-test/f77/attr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/teshsuite/smpi/mpich3-test/f77/attr
-# Build directory: /home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/teshsuite/smpi/mpich3-test/f77/attr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- SimGrid-3.21/tools/graphicator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/tools/graphicator
-# Build directory: /home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/tools/graphicator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(graphicator "/usr/bin/python3" "/home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/bin/tesh" "--ignore-jenkins" "--setenv" "srcdir=/home/kien/project/ipdps2022/fpga_network/SimGrid-3.21" "--setenv" "bindir=/home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/bin" "--cd" "/home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/tools/graphicator" "/home/kien/project/ipdps2022/fpga_network/SimGrid-3.21/tools/graphicator/graphicator.tesh")
