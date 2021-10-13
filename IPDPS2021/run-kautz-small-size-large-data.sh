7842d28c1dbbe53a31789c485fceb82783866a70
KimLoai <ptkcc@outlook.com>
2021-10-13 10:07:52 +0900

    print copy time for cmb algorithm

--- IPDPS2021/run-kautz-small-size-large-data.sh
+++ IPDPS2021/run-kautz-small-size-large-data.sh
@@ -11,17 +11,6 @@ cd multitree-kautz
 make
 topo=(2 3 4 5 ) #6 7 8 ) #9 10 11 12 13 16 32 64)
 numitems=(524288 2621440 6553600 13107200) #5242880)
-echo conges
-for numitem in ${numitems[@]}
-do
-    for d in ${topo[@]}
-    do
-        nproc=$((${d}*(${d} + 1)))
-        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
-                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allgathersmpi --num-item $numitem --algorithm conges
-    done
-done
-
 
 echo cmb
 for numitem in ${numitems[@]}
@@ -33,6 +22,18 @@ do
                 -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allgathersmpi --num-item $numitem --algorithm cmb
     done
 done
+
+echo conges
+for numitem in ${numitems[@]}
+do
+    for d in ${topo[@]}
+    do
+        nproc=$((${d}*(${d} + 1)))
+        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allgathersmpi --num-item $numitem --algorithm conges
+    done
+done
+
 
 echo mttree
 for numitem in ${numitems[@]}
