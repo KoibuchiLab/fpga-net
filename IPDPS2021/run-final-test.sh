dfdba66e0567c520144df4f22c5b065465c841b6
KimLoai <ptkcc@outlook.com>
2021-10-14 21:22:28 +0900

    k16 128MB

--- IPDPS2021/run-final-test.sh
+++ IPDPS2021/run-final-test.sh
@@ -4,20 +4,15 @@ cd multitree-kautz
 make
 echo cmb
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
-                -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm cmb
+../../simgrid-3.28/install/bin/smpirun -np 272 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz16-2.txt \
+                -platform ../platforms/kautz/kautz16-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm cmb
 
 
 echo conges
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
-                -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm conges
-
 
 echo mttree
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
-                -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm mttree
 
 
 cd ..
