6d7e9aff22a7d84e9754fc9233fa696afef49d8c
KimLoai <ptkcc@outlook.com>
2021-10-14 20:19:31 +0900

    change page size

--- IPDPS2021/run-final-test.sh
+++ IPDPS2021/run-final-test.sh
@@ -4,19 +4,19 @@ cd multitree-kautz
 make
 echo cmb
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
+../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094400 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
                 -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm cmb
 
 
 echo conges
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
+../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094400 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
                 -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm conges
 
 
 echo mttree
 
-../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
+../../simgrid-3.28/install/bin/smpirun -np 72 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094400 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz8-2.txt \
                 -platform ../platforms/kautz/kautz8-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm mttree
 
 
@@ -26,5 +26,5 @@ cd multiport-2lvfc
 make allgather
 cd ..
 
-../simgrid-3.28/install/bin/smpirun --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094999 --cfg=smpi/host-speed:1Gf -np 272 -hostfile ./hostfiles/2lvfc/16x17.txt \
+../simgrid-3.28/install/bin/smpirun --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:144245094400 --cfg=smpi/host-speed:1Gf -np 272 -hostfile ./hostfiles/2lvfc/16x17.txt \
                 -platform ./platforms/2lvfc/16x17.xml --log=smpi_coll.:critical ./multiport-2lvfc/allgathersmpi-b --num-item 33554432
