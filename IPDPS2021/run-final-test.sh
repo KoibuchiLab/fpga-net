9d059a45ab75b304fd5ddd43c5432453cfb1f470
KimLoai <ptkcc@outlook.com>
2021-10-15 01:04:35 +0900

    Do lai vi thang k16 64 m nga xuong anh dung

--- IPDPS2021/run-final-test.sh
+++ IPDPS2021/run-final-test.sh
@@ -1,25 +1,21 @@
-
-
 cd multitree-kautz
+
 make
 echo cmb
 
 ../../simgrid-3.28/install/bin/smpirun -np 272 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz16-2.txt \
-                -platform ../platforms/kautz/kautz16-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 33554432 --algorithm cmb
+                -platform ../platforms/kautz/kautz16-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 16777216 --algorithm cmb
 
 
 echo conges
 
+../../simgrid-3.28/install/bin/smpirun -np 272 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz16-2.txt \
+                -platform ../platforms/kautz/kautz16-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 16777216 --algorithm conges
+
 
 echo mttree
 
-
+../../simgrid-3.28/install/bin/smpirun -np 272 --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz16-2.txt \
+                -platform ../platforms/kautz/kautz16-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item 16777216 --algorithm mttree
 
 cd ..
-
-cd multiport-2lvfc
-make allgather
-cd ..
-
-../simgrid-3.28/install/bin/smpirun --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:137438953472 --cfg=smpi/host-speed:1Gf -np 272 -hostfile ./hostfiles/2lvfc/16x17.txt \
-                -platform ./platforms/2lvfc/16x17.xml --log=smpi_coll.:critical ./multiport-2lvfc/allgathersmpi-b --num-item 33554432
