820cb189b9a48645745caed61d7da84d6b2ca748
Kien Pham <ptkien@nii.ac.jp>
2021-11-10 15:01:23 +0900

    some small change

--- IPDPS2021/multitree-kautz/make-scheduletable.sh
+++ IPDPS2021/multitree-kautz/make-scheduletable.sh
@@ -1,5 +1,5 @@
 g++ schedule.cpp helper.cpp -o schedule
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 16 32 64
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 16 #32 64
 do
     ./schedule $i in> scheduleTable/kin$i
     ./schedule $i out> scheduleTable/kout$i
