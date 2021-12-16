4897a66b0192dbcb78a245d506b761f6113bb21d
KimLoai <ptkcc@outlook.com>
2021-12-16 17:12:57 +0900

    test new MPI_isend

--- IPDPS2021/run-kautz-small.sh
+++ IPDPS2021/run-kautz-small.sh
@@ -10,7 +10,7 @@
 cd multitree-kautz
 make
 topo=( 2 4 8 16 ) #32 64) #2 3 4 5 6 7 8 ) #9 10 11 12 13 16 32 64)
-numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 ) #5242880)
+numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288) # 1048576 2097152 ) #5242880)
 
 echo cmb
 for numitem in ${numitems[@]}
@@ -19,7 +19,7 @@ do
     do
         nproc=$((${d}*(${d} + 1)))
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
-                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi-b --num-item $numitem --algorithm cmb
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi --num-item $numitem --algorithm cmb
     done
 done
 
@@ -30,7 +30,7 @@ do
     do
         nproc=$((${d}*(${d} + 1)))
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
-                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi-b --num-item $numitem --algorithm conges
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi --num-item $numitem --algorithm conges
     done
 done
 
@@ -41,6 +41,6 @@ do
     do
         nproc=$((${d}*(${d} + 1)))
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
-                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi-b --num-item $numitem --algorithm mttree
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi --num-item $numitem --algorithm mttree
     done
 done
