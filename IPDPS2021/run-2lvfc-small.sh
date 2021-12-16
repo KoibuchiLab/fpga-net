61a8da31c7d76121e4dd85436a2a2a81788650c9
KimLoai <ptkcc@outlook.com>
2021-12-16 21:10:07 +0900

    test for alltoall 2lvfc

--- IPDPS2021/run-2lvfc-small.sh
+++ IPDPS2021/run-2lvfc-small.sh
@@ -18,14 +18,14 @@ sscanf() {
 }
 
 
-for topo in 2x3 4x5 8x9 #16x17 #32x33 64x65
+for topo in 2x3 4x5 8x9 16x17 #32x33 64x65
     do
-	for numitem in 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
+	for numitem in 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 # 1048576 2097152
 	do
         sscanf $topo "(.*)x(.*)"
         nproc=$((${BASH_REMATCH[1]}*${BASH_REMATCH[2]}))
         ../simgrid-3.28/install/bin/smpirun --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -np $nproc -hostfile ./hostfiles/2lvfc/$topo.txt \
-                -platform ./platforms/2lvfc/$topo.xml --log=smpi_coll.:critical ./multiport-2lvfc/allgathersmpi-b --num-item $numitem
+                -platform ./platforms/2lvfc/$topo.xml --log=smpi_coll.:critical ./multiport-2lvfc/alltoallsmpi-b --num-item $numitem
     done
 done
 
