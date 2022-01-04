5251abd3376a149ce4b2307bf6db62c4165b3849
KimLoai <ptkcc@outlook.com>
2022-01-04 15:55:52 +0900

    get result for allreduce

--- IPDPS2021/run-kautz-small.sh
+++ IPDPS2021/run-kautz-small.sh
@@ -9,7 +9,7 @@
 
 cd multitree-kautz
 make
-topo=( 2 4 8 16 ) #32 64) #2 3 4 5 6 7 8 ) #9 10 11 12 13 16 32 64)
+topo=( 16 ) #32 64) #2 3 4 5 6 7 8 ) #9 10 11 12 13 16 32 64)
 numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 ) #5242880)
 
 echo cmb
@@ -20,7 +20,9 @@ do
         nproc=$((${d}*(${d} + 1)))
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
                 -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm cmb
-    done
+        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
+            -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm cmb
+done
 done
 
 echo conges
@@ -29,6 +31,8 @@ do
     for d in ${topo[@]}
     do
         nproc=$((${d}*(${d} + 1)))
+        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm conges
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
                 -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm conges
     done
@@ -40,6 +44,8 @@ do
     for d in ${topo[@]}
     do
         nproc=$((${d}*(${d} + 1)))
+        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm mttree
         ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
                 -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm mttree
     done
