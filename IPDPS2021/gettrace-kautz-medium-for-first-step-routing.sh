828ab41e94161ebe20057daaf1cad6a37ffa980f
KimLoai <ptkcc@outlook.com>
2022-04-25 09:35:05 +0900

    add --cfg=network/optim:TI to get accuracy result

--- IPDPS2021/gettrace-kautz-medium-for-first-step-routing.sh
+++ IPDPS2021/gettrace-kautz-medium-for-first-step-routing.sh
@@ -15,7 +15,7 @@ topo=( 2 ) #9 10 11 12 13 16 32 64)
 numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152  4194304 8388608 16777216 33554432 67108864 134217728 268435456 )
 
 exe="../../simgrid-3.28/install/bin/smpirun"
-defaultconfig="--cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:429496729600 --cfg=smpi/host-speed:1Gf --log=smpi_coll.:critical -trace -trace-resource"
+defaultconfig=" --cfg=network/optim:TI --cfg=smpi/shared-malloc:global --cfg=smpi/shared-malloc-blocksize:429496729600 --cfg=smpi/host-speed:1Gf --log=smpi_coll.:critical -trace -trace-resource"
 for k in {1..10}
 do
 echo cmb
