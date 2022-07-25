1e818c1d12f1a45cde59b2c59d5d310b4de3164e
KimLoai <ptkcc@outlook.com>
2022-07-25 16:18:09 +0900

    get result 100 times

--- IPDPS2021/run-kautz-small.sh
+++ IPDPS2021/run-kautz-small.sh
@@ -9,20 +9,24 @@
 
 cd multitree-kautz
 make
-topo=( 16 ) #32 64) #2 3 4 5 6 7 8 ) #9 10 11 12 13 16 32 64)
+topo=( 2 ) #32 64) #2 3 4 5 6 7 8 ) #9 10 11 12 13 16 32 64)
 numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 ) #5242880)
-numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152  4194304 8388608 16777216 33554432 67108864 134217728 268435456 )
+numitems=( 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 ) #524288 1048576 2097152  4194304 8388608 16777216 33554432 67108864 134217728 268435456 )
 echo cmb
 for numitem in ${numitems[@]}
 do
     for d in ${topo[@]}
     do
         nproc=$((${d}*(${d} + 1)))
-        ../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
-                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allgathersmpi-b --num-item $numitem --algorithm cmb
+		for i in {1..100}
+		do
+		../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
+                -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./alltoallsmpi-b --num-item $numitem --algorithm cmb
         #../../simgrid-3.28/install/bin/smpirun -np $nproc --cfg=smpi/shared-malloc:local --cfg=smpi/shared-malloc-blocksize:644245094999 --cfg=smpi/host-speed:1Gf -hostfile ../hostfiles/kautz/kautz${d}-2.txt \
         #   -platform ../platforms/kautz/kautz${d}-2.xml --log=smpi_coll.:critical ./allreducesmpi --num-item $numitem --algorithm cmb
-done
+		done
+        
+	done
 done
 
 # echo conges
